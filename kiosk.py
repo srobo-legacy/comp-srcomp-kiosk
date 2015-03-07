af23a322664ed4fc8dee22d3c7a768fce0204f2d
Murray Colpman <muzer@tim32.org>
2015-03-07 19:41:02 +0000

    Disable screensaver, etc.

--- kiosk.py
+++ kiosk.py
@@ -2,6 +2,9 @@
 
 import argparse
 import browser
+import subprocess
+
+# Parse arguments to get the config file location
 
 parser = argparse.ArgumentParser(description='srcomp kiosk system')
 parser.add_argument('--config', dest='config', help='Config file location '
@@ -12,5 +15,12 @@ args = parser.parse_args()
 
 configPath = args.config
 
+# Disable screensaver
+
+subprocess.call(["xset", "-dpms"])
+subprocess.call(["xset", "s", "off"])
+subprocess.call(["xset", "s", "noblank"])
+
+# Start the web browser module
 web = browser.Browser(configPath)
 web.go()
