1f3109c39dbb7a90da9fabd42e1b38dd71061798
Murray Colpman <muzer@tim32.org>
2015-03-28 06:04:25 +0000

    Add missing colons

--- main.pp
+++ main.pp
@@ -6,12 +6,12 @@ package { "unclutter":
   ensure => installed,
 }
 
-file { "/etc/srcomp-kiosk/config.yaml"
+file { "/etc/srcomp-kiosk/config.yaml":
   ensure => file,
   content => "url: '${hiera('url')}'",
 }
 
-file { "/home/pi/.config/autostart/kiosk.desktop"
+file { "/home/pi/.config/autostart/kiosk.desktop":
   ensure => link,
   target => /home/pi/srcomp-kiosk/kiosk.desktop,
   owner => "pi"
