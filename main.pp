6ec3797788b66d983b42d10d902067adc99c6f73
Murray Colpman <muzer@tim32.org>
2015-03-28 06:16:12 +0000

    Fix errors in puppet config

--- main.pp
+++ main.pp
@@ -6,13 +6,24 @@ package { "unclutter":
   ensure => installed,
 }
 
+file { "/etc/srcomp-kiosk":
+  ensure => directory,
+}
+
+$url = hiera('url')
+
 file { "/etc/srcomp-kiosk/config.yaml":
   ensure => file,
-  content => "url: '${hiera('url')}'",
+  content => "url: '${url}'",
+}
+
+file { "/home/pi/.config/autostart":
+  ensure => directory,
+  owner => "pi",
 }
 
 file { "/home/pi/.config/autostart/kiosk.desktop":
   ensure => link,
-  target => /home/pi/srcomp-kiosk/kiosk.desktop,
-  owner => "pi"
+  target => "/home/pi/srcomp-kiosk/kiosk.desktop",
+  owner => "pi",
 }
