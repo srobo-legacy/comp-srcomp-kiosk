f52b6d830903aaeeb75e77884e1c608798456430
Murray Colpman <muzer@tim32.org>
2015-04-11 23:53:54 +0100

    Use - for stdin in deploy-all script

--- deploy-all.sh
+++ deploy-all.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
 
 if [ "$#" != 1 ]; then
-  echo >&2 "Deploys srcomp-kiosk to hostnames given in filename"
+  echo >&2 "Deploys srcomp-kiosk to hostnames given in filename (- for stdin)"
   echo >&2 "Usage: $0 <filename>"
   exit 1
 fi
 
 filename="$1"
+
+if [ "$filename" = "-" ]; then
+  filename=/dev/stdin
+fi
 
 while read -r line; do
   ssh pi@"$line" 'cd srcomp-kiosk; git pull; sudo ./deploy.sh'
