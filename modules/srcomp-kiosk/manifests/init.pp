986a786a02b4a9a35450db1120a17cec5ce04b26
Peter Law <PeterJCLaw@gmail.com>
2015-04-12 14:35:53 +0100

    Tidy quoting, indents per puppet-lint
    Change-Id: I51937cb96f05369f9de3a04909f6904c26e3bda9

--- modules/srcomp-kiosk/manifests/init.pp
+++ modules/srcomp-kiosk/manifests/init.pp
@@ -1,68 +1,68 @@
 class srcomp-kiosk {
 
-  $kioskdir = "/opt/srcomp-kiosk"
+  $kioskdir = '/opt/srcomp-kiosk'
 
-  package { "iceweasel":
+  package { 'iceweasel':
     ensure => installed,
   }
 
-  package { "unclutter":
+  package { 'unclutter':
     ensure => installed,
   }
 
-  package { "python3-yaml":
+  package { 'python3-yaml':
     ensure => installed,
   }
 
-  package { "x11-xserver-utils":
+  package { 'x11-xserver-utils':
     ensure => installed,
   }
 
-  file { "/etc/srcomp-kiosk":
+  file { '/etc/srcomp-kiosk':
     ensure => directory,
   }
 
   $url = hiera('url')
 
-  file { "/etc/srcomp-kiosk/config.yaml":
-    ensure => file,
+  file { '/etc/srcomp-kiosk/config.yaml':
+    ensure  => file,
     content => "url: '${url}'",
   }
 
-  file { "/home/pi/.config/autostart":
-    ensure => directory,
-    owner => "pi",
+  file { '/home/pi/.config/autostart':
+    ensure  => directory,
+    owner   => 'pi',
   }
 
-  file { "/home/pi/.config/autostart/kiosk.desktop":
-    ensure => file,
-    owner => "pi",
-    content => template("srcomp-kiosk/kiosk.desktop.erb"),
+  file { '/home/pi/.config/autostart/kiosk.desktop':
+    ensure  => file,
+    owner   => 'pi',
+    content => template('srcomp-kiosk/kiosk.desktop.erb'),
   }
 
-  file { "/usr/local/bin/srcomp-kiosk":
-    ensure => file,
-    content => template("srcomp-kiosk/srcomp-kiosk.erb"),
-    mode => 0755,
+  file { '/usr/local/bin/srcomp-kiosk':
+    ensure  => file,
+    content => template('srcomp-kiosk/srcomp-kiosk.erb'),
+    mode    => '0755',
   }
 
-  file { "/opt/srcomp-kiosk":
-    ensure => directory,
-    owner => "pi",
+  file { '/opt/srcomp-kiosk':
+    ensure  => directory,
+    owner   => 'pi',
   }
 
-  file { "/opt/srcomp-kiosk/kiosk.py":
-    ensure => file,
-    source => "puppet:///modules/srcomp-kiosk/kiosk.py",
-    mode => 0755,
-    owner => "pi",
+  file { '/opt/srcomp-kiosk/kiosk.py':
+    ensure  => file,
+    source  => 'puppet:///modules/srcomp-kiosk/kiosk.py',
+    mode    => '0755',
+    owner   => 'pi',
   }
 
-  file { "/opt/srcomp-kiosk/firefox-profile":
-    ensure => directory,
+  file { '/opt/srcomp-kiosk/firefox-profile':
+    ensure  => directory,
     recurse => true,
-    source => "puppet:///modules/srcomp-kiosk/firefox-profile",
-    mode => 0755,
-    owner => "pi",
+    source  => 'puppet:///modules/srcomp-kiosk/firefox-profile',
+    mode    => '0755',
+    owner   => 'pi',
   }
 }
