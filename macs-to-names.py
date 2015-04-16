b51fcd531e3963283a89a2a11716c504c4b17b5c
Peter Law <PeterJCLaw@gmail.com>
2015-04-16 22:11:21 +0100

    Add configs for each Pi
    Includes updates to the pi_macs file which records the mapping
    plus the script used to output the configs.
    
    Change-Id: Ifd08e6c592a4ec9af4f158ef2dbf486dc41d02f7

---
+++ macs-to-names.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python
+
+from __future__ import print_function
+
+import os.path
+
+FILE_NAME = 'pi_macs'
+NAME_TEMPLATE = 'pi-{page}-{qual}'
+PAGE_TEMPLATE = 'http://compbox-2015.local/{page}.html{query}'
+CONTENT_TEMPLATE = '''# Student Robotics Pi #{num}
+---
+url: {url}
+hostname: {name}
+'''
+
+def tidy(lines):
+    output_lines = []
+    for line in lines:
+        hash_idx = line.find('#')
+        if hash_idx > -1:
+            line = line[:hash_idx]
+
+        line = line.strip()
+        if line:
+            output_lines.append(line)
+
+    return output_lines
+
+def build_url(page):
+    parts = page.split('?')
+    if len(parts) == 1:
+        return PAGE_TEMPLATE.format(page=page, query='')
+    else:
+        query = '?' + parts[1]
+        return PAGE_TEMPLATE.format(page=parts[0], query=query)
+
+def build_name(num, page):
+    parts = page.split('?')
+    if len(parts) == 1:
+        return NAME_TEMPLATE.format(page=page, qual=num)
+    else:
+        qual = parts[1].replace(',', '')
+        return NAME_TEMPLATE.format(page=parts[0], qual=qual)
+
+def build_filename(mac):
+    return os.path.join('hieradata', 'node', mac + '.yaml')
+
+with open(FILE_NAME, 'r') as fh:
+    lines = tidy(fh.readlines())
+
+for line in lines:
+    num, mac, page = line.split()
+    name = build_name(num, page)
+    url = build_url(page)
+
+    fn = build_filename(mac)
+    with open(fn, 'w+') as fh:
+        fh.write(CONTENT_TEMPLATE.format(name=name, num=num, url=url))
